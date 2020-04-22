ebf18abcd581977c03f0ce0c7bd4890fdd55be20
s18969 <s18969@pjwstk.edu.pl>
2020-04-22 15:34:06 +0200

    Test czy działa poprawnie w VisualStudio

--- Kolokwium_nr1/Startup.cs
+++ Kolokwium_nr1/Startup.cs
@@ -11,7 +11,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace Kolokwium_nr1
+namespace Kolokwium_nr1 //Test czy dziala
 {
     public class Startup
     {
